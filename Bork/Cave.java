8f00215830ed4526e21f9450835541baf68b530d
João Souza <j.souza@philips.com>
2022-06-13 15:18:16 -0300

    Update Cave.java

--- Bork/Cave.java
+++ Bork/Cave.java
@@ -1,8 +1,12 @@
 public class Cave {
     private String description;
-
-    public void setDescription() {
-        this.description = "Descendo as escadas, voce chega a uma caverna subterranea. Nela ha um enorme dragao adormecido. Ele acorda com o barulho, e, vendo voce, prepara-se para desferir um golpe.";
+    
+    public Dragon(String d) {
+        this.setDescription(d);
+    }
+    
+    public void setDescription(String d) {
+        this.description = d;
     }
 
     public String getDescription() {
