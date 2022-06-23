5058030537545d0615768b628ccaa7a5577b550c
Maurício D Lima <72321373+MauricioLim@users.noreply.github.com>
2022-06-23 00:26:57 -0300

    Update Dragon.java

--- Bork/Dragon.java
+++ Bork/Dragon.java
@@ -1,19 +1,20 @@
 /*
 metodo falas do dragão
-arrumar construtor, deixar valores default. (OK)
 */
+import java.util.Random;
 
 public class Dragon {
-    private int health;
     private int damage;
+    private int health;
+    private Random r;
 
-    public Dragon(int h, int d) {
-        this.setHealth(h);
-        this.setDamage(d);
+    public Dragon() {
+        this.setDamage(5);
+        this.setHealth(15);
     }
 
     public void setDamage(int d) {
-        this.damage = 5;
+        this.damage = d;
     }
 
     public void setHealth(int h) {
@@ -25,11 +26,41 @@ public class Dragon {
     }  
     
     public int getHealth() {
-        if (health < 0) {
-            return 0;   
-        }
-        else {
-            return health;
-        }
+        return health;
     }
+
+    public String getDragonSpeech(String n){ // nome da arma
+        String speech="";
+        int numero = r.nextInt(2);
+        if(health==0){
+            if(n.equals("axe")){
+                speech = "C-Com esse machado... pode dizer que cortou o mal pela ra..iz...";
+            } else if(n.equals("sword")){
+                speech = "Essa... é a espada do senhor B-bork solte-a...";
+            } else if(n.equals("fists")){
+                speech = "Nunca fui tão humilhado... morrer para um humano no soco...";
+            }
+        }
+        if(health <= 10 && health >= 1){
+            if(numero == 0){
+                speech = "AAAIIII!! Isso doeu seu fedelho fedorento!";
+            } else if(numero == 1){
+                speech = "Isso vai deixar uma cicatriz.";
+            } else if(numero == 2){
+                speech = "Agora você me deixou bravo!!";
+            }
+        }
+        if(health>=11){
+            if(numero == 0){
+                speech = "Vai ter que fazer melhor que isso se quiser me derrotar!";
+            } else if(numero == 1){
+                speech = "Minha avó bate mais forte que isso!";
+            } else if(numero == 2){
+                speech = "Você precisa ir pra academia garoto.";
+            }
+        }
+
+        return speech;
+    }
+
 }
